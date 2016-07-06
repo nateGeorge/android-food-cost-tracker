8f94ce0dea70e88f639ebb6b30dc947b9e5dbd5b
wordsforthewise <wordsforthewise@gmail.com>
2016-07-06 10:53:37 -0600

    CHORE: initial commit of android project start

---
+++ app/src/main/java/us/ngeorge/foodpricescanner/SnapPhoto.java
@@ -0,0 +1,13 @@
+package us.ngeorge.foodpricescanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SnapPhoto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_snap_photo);
+    }
+}
